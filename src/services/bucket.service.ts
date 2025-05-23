2133b416e090920a67297931ecfcf5885b7585b4
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-05-23 22:16:06 +0530

    feat(service): enhance BucketService with additional methods for bucket access and sharing

--- src/services/bucket.service.ts
+++ src/services/bucket.service.ts
@@ -1,13 +1,17 @@
-import { Bucket } from '../models';
-import { BucketRepository } from '../repositories';
+import { Bucket, StorageObject } from '../models';
+import { BucketRepository, StorageObjectRepository } from '../repositories';
 import { 
     DTOCreateBucketData
 } from '../dtos';
+import mongoose from 'mongoose';
+import { BucketAccessLevel } from '../interface';
 
 class BucketService{
     private bucketRepository:BucketRepository;
+    private storageObjectRepository:StorageObjectRepository;
     public constructor(){
-        this.bucketRepository = new BucketRepository(Bucket)
+        this.bucketRepository = new BucketRepository(Bucket);
+        this.storageObjectRepository = new StorageObjectRepository(StorageObject);
     }
     public async createBucket(
         createBucketData:DTOCreateBucketData):Promise<any>{
@@ -25,9 +29,91 @@ class BucketService{
             );
             return bucket;
         }catch(err){
-            console.error('Registration error:', err);
+            console.error('Error at bucket service(createBucket): ', err);
             throw err;
         } 
+    }
+
+    public async getAllBucketsUserHasAccessTo(userId:string):Promise<any>{
+        try{
+            const buckets = await this.bucketRepository.findAllBucketsOwnerByUser(userId);
+            const sharedBuckets = await this.bucketRepository.findAllBucketsSharedWithUser(userId);
+            return {buckets,sharedBuckets};
+        }catch(err){
+            console.error('Error at bucket service(getAllBucketsUserHasAccessTo): ', err);
+            throw err;
+        } 
+    }
+
+    public async getBucketById(userId:string,bucketId:string){        
+        try{
+            if(!mongoose.Types.ObjectId.isValid(bucketId)){
+                throw new Error('BUCKET_ERROR2');
+            };
+            const bucket = await this.bucketRepository.findBucketById(bucketId);
+            if(!bucket){
+                throw new Error('BUCKET_ERROR3');
+            }
+        
+            const isOwner = bucket.owner.toString() === userId.toString();
+            
+            const isShared = 
+                    bucket.accessLevel === BucketAccessLevel.SHARED && 
+                    bucket.sharedWith?.some(id => id.toString() === userId.toString());
+
+            const isPublic = bucket.accessLevel === BucketAccessLevel.PUBLIC;
+
+        
+            if(!isOwner && !isShared && !isPublic){
+                throw new Error('BUCKET_ERROR4');
+            }
+        
+            const objectCount = await this.storageObjectRepository.getCountOfAllObjectsInBucket(bucketId)
+        
+            // Calculate total storage used in this bucket
+            const storageUsed = await this.storageObjectRepository.getTotalStorageUsedInBucket(bucketId);
+        
+            const totalSize = storageUsed.length > 0 ? storageUsed[0].total : 0;
+            return {bucket,totalSize,objectCount,isOwner};
+        }catch(err){
+            console.error('Error at bucket service(getBucketById): ', err);
+        }
+    }
+
+    public async shareBucketWithUser(bucketId:string,userIds:string[],userId:string){
+        try{
+            if(!this.validateBucketId(bucketId)){
+                throw new Error('BUCKET_ERROR2');
+            };
+            const bucket = await this.bucketRepository.findBucketById(bucketId);
+            if(!bucket){
+                throw new Error('BUCKET_ERROR3');
+            }
+            if(bucket.owner.toString() !== userId.toString()){
+                throw new Error('You do not have permission to share this bucket');
+            }
+
+            // updating bucket access level to SHARED if it's not already
+            bucket.accessLevel = BucketAccessLevel.SHARED;
+
+            // adding users to the sharedWith array (avoiding duplicates)
+            if(!bucket.sharedWith){
+                bucket.sharedWith = [];
+            }
+            userIds.forEach((id: string) => {
+                if(mongoose.Types.ObjectId.isValid(id) && 
+                !bucket.sharedWith?.some(existingId => existingId.toString() === id)){
+                    bucket.sharedWith?.push(id as any);
+                }
+            });
+            await bucket.save();
+            return bucket;
+        }catch(err){
+            throw err;
+        }
+    }
+    private validateBucketId(bucketId:string):boolean{
+        return mongoose.Types.ObjectId.isValid(bucketId);
     }
 }
 
